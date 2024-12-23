cc5174d8e805c026f411afdc89af5af379c2b886
TJOEUNIS <minsung712@yahoo.co.kr>
2024-12-23 13:20:51 +0900

    initial commit

---
+++ src/main/java/kms/Previous/exam/questions/PreviousExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package kms.Previous.exam.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreviousExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreviousExamQuestionsApplication.class, args);
+	}
+
+}
